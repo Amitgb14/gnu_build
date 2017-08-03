1592da4c2907082a532005ef391040ff7d918195
Amitgb14 <amitg.b14@gmail.com>
2017-08-03 16:46:11 +0530

    First commit

---
+++ gnu_src/build.py
@@ -0,0 +1,26 @@
+
+import os
+from configparser import Configparser
+
+def read_config():
+	pass
+
+def setup();
+	pass
+
+def download():
+	pass
+
+def initialize():
+	pass
+
+def start_build():
+	pass
+
+def start_test():
+	pass
+
+def main():
+	pass
+
+
